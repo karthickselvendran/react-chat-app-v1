d4189762ac84ee26aaec8147aea5f09ad8e6f98f
karthickselvendran <selvendrankarthick@gmail.com>
2022-04-10 19:29:53 +0530

    loading added

--- src/pages/chat/chat.js
+++ src/pages/chat/chat.js
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Loading from '../../components/Loading/Loading';
 import io from 'socket.io-client';
+import './chat.css';
 
 let socket;
 
 export const Chat = () => {
     const navigate = useNavigate();
+    const [loadingStatus, setLoadingStatus] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoadingStatus(false)
+        }, 2000)
+    }, [])
+
     useEffect(() => {
         const userData = JSON.parse(localStorage.getItem('userData'))
         if (!userData) {
@@ -19,10 +29,19 @@ export const Chat = () => {
     useEffect(() => {
         socket = io(backEndUrl)
     }, [])
-
+    console.log(loadingStatus)
     return (
-        <>
-            <h3>Chat page</h3>
-        </>
+        <div>
+            {
+                loadingStatus ?
+                    <Loading /> :
+                    <div className='chatPage'>
+                        <div className=''>
+                            <h3>Chat page</h3>
+                        </div>
+                    </div>
+            }
+        </div>
+
     )
 }
